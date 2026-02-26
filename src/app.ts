e5374bf16205468a504a9b0468fe3359759b3002
LeeGMin <yumi3545@naver.com>
2026-02-26 08:05:19 +0000

    feat: 실시간 알람 설정 추가

--- src/app.ts
+++ src/app.ts
@@ -7,8 +7,11 @@ import { errorHandler } from './middlewares/errorHandler';
 import commentRouter from './router/comment.router';
 import authRouter from './router/auth.router';
 import userRouter from './router/user.router';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import notificationRouter from './router/notification.router';
 
 const app = express();
 
@@ -27,12 +30,35 @@ app.use('/product', productRouter);
 app.use('/comment', commentRouter);
 app.use('/auth', authRouter);
 app.use('/users', userRouter);
+app.use('/notifications', notificationRouter);
 
 app.use(errorHandler);
 
+const httpServer = createServer(app);
+
+export const io = new Server(httpServer, {
+  cors: {
+    origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+    credentials: true,
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log('유저 연결됨:', socket.id);
+
+  socket.on('join', (userId) => {
+    socket.join(`user_${userId}`);
+    console.log(`유저 ${userId}가 방에 입장했습니다.`);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('유저 연결 끊김:', socket.id);
+  });
+});
+
 const PORT = 3000;
 
-app.listen(PORT, () => {
+httpServer.listen(PORT, () => {
   console.log(`🚀 Server is running on http://localhost:${PORT}`);
 });
 
